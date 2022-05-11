1680f8abe26b8ebe646f5d4881bab246376f0940
Cinthia M. Orona <devrider2021@gmail.com>
2022-05-11 07:02:04 -0300

    Primer ejercicio de la Clase5
    Escribir un programa que haga lo siguiente:
    
    1) Borrar la pantalla.
    2) Pedir el nombre de una persona.
    3) Saludarlo con un texto que diga "¡Hola [NombreIngresado]!"
    4) Preguntar si se quiere continuar.
    5) Si la respuesta es "S" repetir desde el punto 1.
    6) Si la respuesta es "N" finalizar el programa mostrando un mensaje que diga "Programa finalizado correctamente".
    7) Si la respuesta no es ni "S" ni "N" que tambien finalice el programa, pero mostrando un mensaje que diga "Opcion no valida".

---
+++ Clase5_Tarea/PrimerEjercicio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace PrimerEjercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Hola, si quiere continuar ingrese: (S/N)");
+            string respuesta = Console.ReadLine();
+
+            respuesta = "S";
+            int contador = 0;
+
+            while(respuesta.ToUpper()!="N")
+            {
+                contador++;
+                Console.WriteLine("Ingrese su nombre");
+                string nombre = Console.ReadLine();
+
+                Console.WriteLine($"¡Hola {nombre}!");
+                Console.ReadKey();
+
+                Console.WriteLine("Quiere continuar (S/N)");
+                respuesta = Console.ReadLine();                
+            }
+            
+            if (respuesta.ToUpper()!="S")
+            {
+                Console.WriteLine("Programa finalizado correctamente");
+            }
+            else
+            {
+                Console.WriteLine("Opcion no valida");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
