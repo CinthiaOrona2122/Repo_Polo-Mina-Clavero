7639847ea8630c7d9819f97540f747ec43998adf
Cinthia M. Orona <devrider2021@gmail.com>
2022-05-14 23:30:45 -0300

    Segundo Ejercicio de la Clase 6
    int numeroSecreto = new
    Random(DateTime.Now.Millisecond).Next(1, 21);
    
    2) Pedir al usuario que ingrese un número y que
    intente adivinar el número que eligió la computadora.
    
    3) Si el numero ingresado por el usuario es mayor
    al número secreto, avisarle que es muy grande y que intente de nuevo y que
    vuelva al paso 2.
    
    4) Si el numero ingresado es menor al número
    secreto, avisarle que es muy chico, y que intente de nuevo volviendo al paso 2.
    
    5) Si el número ingresado coincide con el número secreto, sacar el siguiente mensaje:
    
    "Felicitaciones, has adivinado el número
    secreto que era:  [numeroSecreto]"
    "Lo has logrado en [n] intentos!!"
    
    Ejercicio 2 NET
    
    Nota:
    Obviamente hay que hacer un bucle y crear un contador de intentos.
    Sean honestos con ustedes mismos y usen sus propias neuronas para resolver el problema, y aunque no lo terminen lo importante es intentarlo.

---
+++ Clase6_Tarea/NumeroRandom/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NumeroRandom
+{
+    internal class Program
+    {
+        static void Main(string[] arg)
+        {
+            int numeroSecreto = new Random(DateTime.Now.Millisecond).Next(1, 5);
+            int contador = 0;
+
+            Console.WriteLine("Hola, ingresa un numero: ");
+            int numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+
+
+            while (numeroIngresado != numeroSecreto)
+            {
+                Console.WriteLine("Intenta otro numero");
+                numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+                if (numeroIngresado > numeroSecreto)
+                {
+                    Console.WriteLine("Oops!! el numero ingresado es mayor al secreto");
+                }
+                else
+                {
+                    Console.WriteLine("Oops!! el numero ingresado es menor al secreto");
+                }
+            }
+
+            if (numeroIngresado == numeroSecreto)
+            {
+                Console.WriteLine($"Felicitaciones, has adivinado el número secreto que era:  {numeroSecreto}");
+                Console.WriteLine($"Lo has logrado en {contador} intentos!!");
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
+
