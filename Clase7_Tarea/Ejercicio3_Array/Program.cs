ab814f395911d41e4b672d3c07cf3ddc1aa0850f
Cinthia M. Orona <devrider2021@gmail.com>
2022-05-19 15:34:28 -0300

    Tercer Ejercicio de la Clase 7
    1) Pida 10 números al usuario.
    2) Obtener la suma de todos los números ingresados.
    3) Obtener cual es el mayor de todos los números.
    4) Obtener cual es el menor de todos los números.
    5) Obtener el promedio de todos los números.
    6) Mostrar todos los números ingresados por pantalla.
    7) Mostrar los valores calculados en los puntos 2, 3, 4 y 5.

---
+++ Clase7_Tarea/Ejercicio3_Array/Program.cs
@@ -0,0 +1,60 @@
+﻿
+int[] numero = new int[10];
+int contador = 0;
+int numMayor = 0;
+int numMenor = 0;
+int promedio = 0;
+
+//guardar numeros en los indices
+Console.WriteLine($"El array tiene {numero.Length} indices.");
+
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine(i+1 + " : " + "Ingrese un numero:");
+    numero[i] = Convert.ToInt32(Console.ReadLine());
+
+    Console.Clear();
+    Console.WriteLine($"Ultimo numero ingresado es: {numero[i]}");
+}
+
+//sumar todos los indices
+for (int i = 0; i < numero.Length; i++)
+{
+    contador += numero[i];
+    promedio = (contador / numero.Length);
+}
+Console.WriteLine();
+Console.WriteLine($"El promedio del array es: {promedio}");
+Console.WriteLine();
+Console.WriteLine($"El valor de la suma es: {contador}");
+Console.WriteLine();
+Console.Write($"Los valores del array son: ");
+
+//mostrar todos los indices ingresados
+for (int i = 0; i < numero.Length; i++)
+{
+    Console.Write(numero[i]+ ", ");
+}
+
+//el mayor y menor ...
+for (int i = 0; i < numero.Length; i++)
+{
+    if(i == 1 - 1)
+    {
+        numMayor = numero[i];
+        numMenor = numero[i];
+    }
+    else if(numero[i] > numMayor)
+    {
+        numMayor = numero[i];
+    }
+    else
+    {
+        numMenor = numero[i];
+    }
+
+}
+Console.WriteLine($"El numero mayor es: {numMayor}");
+Console.WriteLine($"El numero menor es: {numMenor}");
+Console.ReadKey();
